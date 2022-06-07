cdc13f523317bd8e5046361cc84abef8d950cef2
Marc Jurriens <marc_uva@DESKTOP-6Q1LGFB.localdomain>
2022-06-07 15:09:43 +0200

    fill_schedule_baseline.py before pull

--- code/fill_schedule_baseline.py
+++ code/fill_schedule_baseline.py
@@ -1,20 +1,23 @@
 import pandas as pd
+from empty_schedule import build_empty_schedule
 
-# make a list of 
-course_data = pd.read_csv('../data/courses.csv', delimiter=";")
-courses = list(course_data["Vakken voor periode 4"])
+test = build_empty_schedule()
+print(test)
+# # make a list of 
+# course_data = pd.read_csv('../data/courses.csv', delimiter=";")
+# courses = list(course_data["Vakken voor periode 4"])
 
-schedule = pd.read_csv('../data/schedule.csv')
+# schedule = pd.read_csv('../data/schedule.csv')
 
-x = 1
-y = 0
-for course in courses:
-    schedule.iloc[y,x]= course
-    x += 1
-    if x == 8:
-        y += 1
-        x = 1
+# x = 1
+# y = 0
+# for course in courses:
+#     schedule.iloc[y,x]= course
+#     x += 1
+#     if x == 8:
+#         y += 1
+#         x = 1
 
-print(schedule) 
+# print(schedule) 
 
-schedule.to_csv("../data/schedule_baseline.csv")
+# schedule.to_csv("../data/schedule_baseline.csv")
