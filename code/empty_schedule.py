ae8b4e6348000af637e7ff63ec56e4e8d78b2e7d
Dennis Vlegels <dennis.vlegels@hotmail.com>
2022-06-07 14:43:03 +0200

    Changed schedule.py into empty_schedule.py as function

---
+++ code/empty_schedule.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import loader
+
+
+def build_empty_schedule(rooms):
+
+    # load in lists of all room objects
+    rooms = loader.init_rooms("../data/rooms.csv")
+
+    # build list of all possible time slots
+    timeslots = ["Monday 09:00-11:00", "11:00-13:00", "13:00-15:00", "15:00-17:00", 
+                "Tuesday 09:00-11:00", "11:00-13:00", "13:00-15:00", "15:00-17:00", 
+                "Wednesday 09:00-11:00", "11:00-13:00", "13:00-15:00", "15:00-17:00", 
+                "Thurday 09:00-11:00", "11:00-13:00", "13:00-15:00", "15:00-17:00", 
+                "Friday 09:00-11:00", "11:00-13:00", "13:00-15:00", "15:00-17:00", ]
+
+    # build schedule and return
+    schedule = pd.DataFrame(index = timeslots, columns=rooms, data=0)
+    return schedule
+
+# # save schedule
+# schedule.to_csv("../data/schedule.csv")
+
+# # welke lokalen vrij op maandag 9-11?
+# sub_df = (data.iloc[0] == 0)
+# print(sub_df)
