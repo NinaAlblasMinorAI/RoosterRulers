91951d745edccc1d19a42c0a4cabfc045318cae0
Nina <ninamalblas@gmail.com>
2022-06-27 22:32:02 +0200

    lekker bezig met code herschrijven

--- code/visualization/visualize_box_plot.py
+++ code/visualization/visualize_box_plot.py
@@ -32,46 +32,5 @@ def visualize_box_plot(list_of_scores_hc, list_of_scores_sa, N, T, R1, R2, R3):
                         f"$\\bfSimulatedAnnealing$\nAverage: {round(average_sa)}\nMinimum: {minimum_sa}"])
     ax.set_title(f"Objective value of the valid solutions\nof the hillclimber and simulated annealing algorithms\n(T = {T}, $R_1$ = {R1}, $R_2$ = {R2}, $R_3$ = {R3}, N = {N})", fontweight="bold")
 
-
-
-
-
-
-
-    # fig = plt.figure(figsize=(10, 8))
-    # gs = gridspec.GridSpec(nrows=1, ncols=2)#, width_ratios=[1,1], wspace=0.5)
-
-    # # hillclimber
-    # average_hc = mean(list_of_scores_hc)
-    # minimum_hc = min(list_of_scores_hc)
-    # maximum_hc = max(list_of_scores_hc)
-    
-    # ax1 = fig.add_subplot(gs[0])
-    # ax1.boxplot(list_of_scores_hc)
-    # ax1.tick_params(axis='y')#, labelcolor=color)
-    # ax1.set_title(f"Objective value of the valid solutions\nof the hillclimber algorithm\n(N = {N})")
-    # ax1.set_xticks([])
-    # ax1.set_xlabel(f"Average: {round(average_hc)}\nMinimum: {minimum_hc}")
-    # ax1.set_ylabel("Objective value")
-
-    # # simulated annealing
-    # average_sa = mean(list_of_scores_sa)
-    # minimum_sa = min(list_of_scores_sa)
-    # maximum_sa = max(list_of_scores_sa)
-
-    # ax2 = fig.add_subplot(gs[1])
-    # ax2.boxplot(list_of_scores_sa)
-    # ax2.tick_params(axis='y')#, labelcolor=color)
-    # ax2.set_title(f"Objective value of the valid solutions\nof the simulated annealing algorithm\n(N = {N})")
-    # ax2.set_xticks([])
-    # ax2.set_xlabel(f"Average: {round(average_sa)}\nMinimum: {minimum_sa}")
-    # ax2.set_ylabel("Objective value")
-
-    # y_min = min(minimum_hc, minimum_sa)
-    # y_max = max(maximum_hc, maximum_sa)
-
-    # ax1.set_ylim(y_min - 1, y_max + 1)
-    # ax2.set_ylim(y_min - 1, y_max + 1)
-
     
     plt.savefig(f"output_data/{dt_string}_boxplot.png", dpi=300)
--- code/visualization/visualize_schedule.py
+++ code/visualization/visualize_schedule.py
@@ -1,109 +1,101 @@
-import numpy as np
-from bokeh.io import curdoc
-from bokeh.plotting import figure, save, output_file
-from bokeh.models import ColumnDataSource, Grid, LinearAxis, Plot, Rect, Text, HoverTool, DataTable, HTMLTemplateFormatter, TableColumn, LinearColorMapper, CategoricalColorMapper, CustomJS, TapTool
-from bokeh.layouts import column, row
-from bokeh.palettes import RdYlGn
-import pandas as pd
 from code.classes.lesson import Lesson
+import numpy as np
+import pandas as pd
 import itertools
+from bokeh.io import curdoc
+from bokeh.plotting import save, output_file
+from bokeh.models import ColumnDataSource, Grid, LinearAxis, Plot, Rect, Text
+from bokeh.models import HoverTool, DataTable, HTMLTemplateFormatter
+from bokeh.models import LinearColorMapper, CustomJS, TableColumn, TapTool
+from bokeh.layouts import row
+from bokeh.palettes import RdYlGn
+
 
 def visualize_schedule(schedule_obj, output_file_path):
+    """
+    Takes a Schedule instance and visualizes it into 
+    an HTML file using the Bokeh library.
+    """
 
+    # specify where to store HTML schedule
     output_file(output_file_path)
     
+    # add malus points to individual lessons and students
     schedule_obj.eval_schedule_objects()
 
-    # create the plot on which we design the roster
-    roster = Plot(width=2300, height=1580, title="ROOSTER RULERS' SCHEDULE")
-
-    roster.add_tools(TapTool())
+    # create the plot on which we design the schedule
+    bokeh_schedule = Plot(width=2300, height=1580, title="ROOSTER RULERS' SCHEDULE")
 
     # retrieve the dataframe of the schedule object
     schedule_df = schedule_obj.get_dataframe()
 
-    # get the minimum and maximum malus points
-    malus_points_df = schedule_df.applymap(lambda lesson_obj:lesson_obj.get_malus_points() if isinstance(lesson_obj, Lesson) else np.nan)
-    min_malus_points = int(malus_points_df.min().min())
-    max_malus_points = int(malus_points_df.max().max())
-
-    nr_time_slots = 5
-    number_of_rooms = len(schedule_obj.get_rooms()) # 7
-
+    # determine amount of time slots and rooms
+    time_slots_per_day = 5
     days_of_the_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
-    # all_days = [day for day in days_of_the_week for time_slot in range(number_of_rooms * nr_time_slots)]
-    total_time_slots = len(days_of_the_week) * nr_time_slots # 25
+    total_time_slots = len(days_of_the_week) * time_slots_per_day
+    number_of_rooms = len(schedule_obj.get_rooms())
 
-    x_values = np.tile(np.arange(number_of_rooms), total_time_slots)     # 0 1 2 3 4 5 6 0 1 2 3 4 5 6 ...
-    y_values = np.repeat(np.arange(total_time_slots) + .25, number_of_rooms)   # 0.5 0.5 0.5 0.5 0.5 0.5 0.5 1.5 1.5 1.5 1.5 1.5 1.5 1.5 ...
+    # create coordinates of the rectangles (lessons) in the schedule
+    x_values = np.tile(np.arange(number_of_rooms), total_time_slots)
+    y_values = np.repeat(np.arange(total_time_slots) + .25, number_of_rooms)
 
-    # dictionary of lists of the lesson attributes in the order the rectangles are created in
-    lesson_dict = lesson_attributes(schedule_df)
-    lesson_names = lesson_dict["Name"]
-    lesson_types = lesson_dict["Type"]
-    lesson_group_nrs = lesson_dict["Group nr."]
-    lesson_nr_students = lesson_dict["Nr. students"]
-    lesson_students = lesson_dict["Students"]
-    lesson_malus_points = lesson_dict["Malus points"]
-    lesson_mp_conflicts = lesson_dict["MP conflicts"]
-    lesson_mp_gaps = lesson_dict["MP gaps"]
-    lesson_mp_capacity = lesson_dict["MP capacity"]
-    lesson_mp_evening = lesson_dict["MP evening"]
-    lesson_all_days = lesson_dict["Days"]
+    # dictionary of each lesson's attributes
+    attributes = lesson_attributes(schedule_df)
 
-    # TODO: rename these variables
-    todays_x, todays_y = remove_empty_slots(schedule_obj, x_values, y_values)
+    # only keep coordinates with lessons assigned to them
+    x_lessons, y_lessons = remove_empty_slots(schedule_obj, x_values, y_values)
 
     # width and height of each lesson's visual representation (rectangle)
-    width = np.full(len(todays_x), .98)
-    height = np.full(len(todays_x), .95)
+    rectangle_width = np.full(len(x_lessons), .98)
+    rectangle_height = np.full(len(x_lessons), .95)
 
-    # add one day's rectangles to roster
-    rect_source = ColumnDataSource(data=dict(x=todays_x, 
-                                        y=todays_y, 
-                                        w=width, 
-                                        h=height,
-                                        types=lesson_types,
-                                        group_nrs=lesson_group_nrs,
-                                        nr_students=lesson_nr_students,
-                                        conflict_points=lesson_mp_conflicts,
-                                        gap_points=lesson_mp_gaps,
-                                        capacity_points=lesson_mp_capacity,
-                                        evening_points=lesson_mp_evening,
-                                        days=lesson_all_days))
+    # create data source for rectangles
+    rect_source = ColumnDataSource(dict(x=x_lessons, 
+                                        y=y_lessons, 
+                                        w=rectangle_width, 
+                                        h=rectangle_height,
+                                        types=attributes["Type"],
+                                        group_nrs=attributes["Group nr."],
+                                        nr_students=attributes["Nr. students"],
+                                        conflict_points=attributes["MP conflicts"],
+                                        gap_points=attributes["MP gaps"],
+                                        capacity_points=attributes["MP capacity"],
+                                        evening_points=attributes["MP evening"],
+                                        days=attributes["Days"],
+                                        colors=attributes["Colors"]))
 
-    rect_colormapper = CategoricalColorMapper(factors=["0", "1", "2", "3", "4"], palette=["lightcyan", "darkturquoise", "lightcyan", "darkturquoise", "lightcyan"])
+    # add the lesson rectangles to the plot
+    rectangles = Rect(x="x", y="y", width="w", height="h", fill_color="colors")
+    all_rectangles = bokeh_schedule.add_glyph(rect_source, rectangles)
 
-
-    rectangles = Rect(x="x", y="y", width="w", height="h", fill_color={'field': 'days', 'transform': rect_colormapper})
-    all_rectangles = roster.add_glyph(rect_source, rectangles)
-
-    small_rect_source = ColumnDataSource(dict(x=todays_x + .4, 
-                                        y=todays_y, 
-                                        w=width / 7.1, 
-                                        h=height / 1.75,
-                                        points=lesson_malus_points,))
+    small_rect_source = ColumnDataSource(dict(x=x_lessons + .4, 
+                                                y=y_lessons, 
+                                                w=rectangle_width / 7.1, 
+                                                h=rectangle_height / 1.75,
+                                                points=attributes["Malus points"]))
 
     # color the small rectangles
     small_rect_colormapper = LinearColorMapper(palette=RdYlGn[3], 
-                                    low=min_malus_points, 
-                                    high=max_malus_points)
+                                    low=min(attributes["Malus points"]), 
+                                    high=max(attributes["Malus points"]))
 
     small_rectangles = Rect(x="x", y="y", width="w", height="h", fill_color={'field': 'points', 'transform': small_rect_colormapper})
-    roster.add_glyph(small_rect_source, small_rectangles)
+    bokeh_schedule.add_glyph(small_rect_source, small_rectangles)
+
+    lesson_names = attributes["Name"]
 
     # add course name text       # hier wil je niet de lege waarden uithalen want die maak ik al lege strings!
     text_source = ColumnDataSource(dict(x=x_values - .45, y=y_values + .35, text=lesson_names))
     lesson_text = Text(x="x", y="y", text="text")
     lesson_text.text_font_size = {'value': '13px'}
-    roster.add_glyph(text_source, lesson_text)
+    bokeh_schedule.add_glyph(text_source, lesson_text)
 
     # add malus point text
-    malus_points_text_source = ColumnDataSource(dict(x=todays_x + .37, y=todays_y + .1, text=lesson_malus_points))
+    malus_points_text_source = ColumnDataSource(dict(x=x_lessons + .37, y=y_lessons + .1, text=attributes["Malus points"]))
     malus_points_text = Text(x="x", y="y", text="text")
     malus_points_text.text_font_size = {'value': '13px'}
     malus_points_text.text_font_style = {'value': 'bold'}
-    roster.add_glyph(malus_points_text_source, malus_points_text)
+    bokeh_schedule.add_glyph(malus_points_text_source, malus_points_text)
 
     # create the students data table in bokeh
     html_formatter_name = HTMLTemplateFormatter(template="""
@@ -132,9 +124,12 @@ def visualize_schedule(schedule_obj, output_file_path):
                             height=1500,
                             autosize_mode="none")
 
+    bokeh_schedule.add_tools(TapTool())
     all_rectangles.selection_glyph = Rect(fill_color="deeppink")
 
     lesson_name_list = list(filter(lambda value: value !=  None, lesson_names))
+
+    lesson_students = attributes["Students"]
 
     rect_source.selected.js_on_change('indices', CustomJS(
         args=dict(s1=student_source, 
@@ -184,39 +179,6 @@ def visualize_schedule(schedule_obj, output_file_path):
         """)
     )
 
-
-
-
-
-    # def print_selected_inds(attrname, old, new):
-    #     # print(rect_source.selected.indices)
-    #     pass
-
-    # rect_source.selected.on_change("indices", print_selected_inds)
-
-    # def country_select(attrname, old, new):
-    #     rect_source.data = get_data(rect_source.selected.indices, country_dict)
-
-    # rect_source.selected.on_change('indices')
-
-    # students_source = ColumnDataSource(dict(
-    #                                     dates=[i + 10 for i in range(10)],
-    #                                     downloads=[i + 100 for i in range(10)],
-    #                                 ))
-
-    # print(dict(
-    #             dates=[i + 10 for i in range(10)],
-    #             downloads=[i + 100 for i in range(10)],
-    #         ))
-
-    # print(schedule_obj.get_student_name_dict())
-
-
-
-
-
-
-
     # hover tool that only works for the rectangles
     hover = HoverTool(renderers=[all_rectangles])
     hover.tooltips = """
@@ -232,42 +194,42 @@ def visualize_schedule(schedule_obj, output_file_path):
         </div>
     """
 
-    roster.add_tools(hover)
+    bokeh_schedule.add_tools(hover)
         
     # add x and y axis - ???
     xaxis = LinearAxis(axis_label="Rooms | Capacity")
-    roster.add_layout(xaxis, 'above')
+    bokeh_schedule.add_layout(xaxis, 'above')
     yaxis = LinearAxis(axis_label='Time slots')
-    roster.add_layout(yaxis, 'left')
+    bokeh_schedule.add_layout(yaxis, 'left')
     yaxis2 = LinearAxis(axis_label='Days')
-    roster.add_layout(yaxis2, 'left')
+    bokeh_schedule.add_layout(yaxis2, 'left')
 
     # ???
-    roster.add_layout(Grid(dimension=0, ticker=xaxis.ticker))
-    roster.add_layout(Grid(dimension=1, ticker=yaxis.ticker))
-    roster.add_layout(Grid(dimension=1, ticker=yaxis2.ticker))
+    bokeh_schedule.add_layout(Grid(dimension=0, ticker=xaxis.ticker))
+    bokeh_schedule.add_layout(Grid(dimension=1, ticker=yaxis.ticker))
+    bokeh_schedule.add_layout(Grid(dimension=1, ticker=yaxis2.ticker))
 
     # create room tick labels
-    roster.xaxis.major_label_overrides = dict(zip(range(7), [str(elem) for elem in schedule_obj.get_rooms()]))
-    roster.xaxis.minor_tick_line_color = None
+    bokeh_schedule.xaxis.major_label_overrides = dict(zip(range(7), [str(elem) for elem in schedule_obj.get_rooms()]))
+    bokeh_schedule.xaxis.minor_tick_line_color = None
 
     # make sure we start at 0 in the upper left corner
-    roster.y_range.flipped = True 
+    bokeh_schedule.y_range.flipped = True 
 
     # create time tick labels (0 up to and including 25, to get a "19:00" tick)         
-    roster.yaxis[0].ticker = np.arange(total_time_slots + 1)
-    roster.yaxis[0].major_label_overrides = {num : time_ticker_func(num) for num in range(total_time_slots + 1)}
+    bokeh_schedule.yaxis[0].ticker = np.arange(total_time_slots + 1)
+    bokeh_schedule.yaxis[0].major_label_overrides = {num : time_ticker_func(num) for num in range(total_time_slots + 1)}
 
     # create day tick labels (starting at 2.5 with steps of 5, to get ticks halfway through each day)
-    roster.yaxis[1].ticker = np.arange((nr_time_slots / 2), total_time_slots + (nr_time_slots / 2), len(days_of_the_week))
-    roster.yaxis[1].major_label_overrides = {num : day_ticker_func(num) for num in np.arange((nr_time_slots / 2), total_time_slots + (nr_time_slots / 2), len(days_of_the_week))}
+    bokeh_schedule.yaxis[1].ticker = np.arange((time_slots_per_day / 2), total_time_slots + (time_slots_per_day / 2), len(days_of_the_week))
+    bokeh_schedule.yaxis[1].major_label_overrides = {num : day_ticker_func(num) for num in np.arange((time_slots_per_day / 2), total_time_slots + (time_slots_per_day / 2), len(days_of_the_week))}
 
-    roster.xaxis.axis_label_text_font_size = "15px"
-    roster.xaxis.major_label_text_font_size = "15px"
-    roster.yaxis.axis_label_text_font_size = "15px"
-    roster.yaxis.major_label_text_font_size = "15px"
+    bokeh_schedule.xaxis.axis_label_text_font_size = "15px"
+    bokeh_schedule.xaxis.major_label_text_font_size = "15px"
+    bokeh_schedule.yaxis.axis_label_text_font_size = "15px"
+    bokeh_schedule.yaxis.major_label_text_font_size = "15px"
 
-    layout = row(roster, data_table)
+    layout = row(bokeh_schedule, data_table)
 
     curdoc().add_root(layout)
 
@@ -301,6 +263,9 @@ def day_ticker_func(tick_value):
 
     return tick_to_day_dict[tick_value]
 
+def get_colors():
+    ["lightcyan" if day_index % 2 == 0 else "darkturquoise" if day_index != 1 else None for day_index in range(5) for _ in range(35)]
+
 def lesson_attributes(lessons_df):
     """
     Returns dictionary of the lesson attributes,
@@ -312,8 +277,7 @@ def lesson_attributes(lessons_df):
     lessons_list = list(itertools.chain(*lessons_df.values.tolist()))
 
     # names
-    lesson_names_list = list(map(lambda lesson_obj:lesson_obj.get_name() if isinstance(lesson_obj, Lesson) else None, lessons_list))
-    attributes["Name"] = lesson_names_list
+    attributes["Name"] = list(map(lambda lesson_obj:lesson_obj.get_name() if isinstance(lesson_obj, Lesson) else None, lessons_list))
    
     # types
     lesson_types_list = list(map(lambda lesson_obj:lesson_obj.get_type() if isinstance(lesson_obj, Lesson) else "", lessons_list))
@@ -356,6 +320,21 @@ def lesson_attributes(lessons_df):
     days_list = list(map(lambda lesson_obj:str(lesson_obj.get_day()) if isinstance(lesson_obj, Lesson) else "", lessons_list))
     attributes["Days"] = list(filter(lambda value: value !=  "", days_list))
 
+    # colors
+    colors = []
+    slot_number = 0
+    for day_index in range(5): # TODO: niet hardcoden?
+        for day_slot in range(35): # TODO: niet hardcoden?
+            if isinstance(lessons_list[slot_number], Lesson):
+                if day_index % 2 == 0:
+                    colors.append("lightcyan")
+                else:
+                    colors.append("darkturquoise")
+
+            slot_number += 1
+
+    attributes["Colors"] = colors
+
     return attributes
 
 def get_all_empty_slots(schedule_obj):
@@ -379,15 +358,5 @@ def remove_empty_slots(schedule_obj, x_vals, y_vals):
     return x_vals, y_vals + .25
 
 ### TODO
-# Als je op student klikt: zie zijn rooster
-# - ik denk dat ik OF JScode moet gebruiken OF een bokeh server
-# - state maken van het rooster in z'n geheel, zodat je er altijd weer naartoe terug kunt. 
-# - Dan van individuele roosters hele nieuwe states maken, die veranderen de 'value' als .selected
-# - OF dan maken we de geselecteerde dingen een andere kleur!
-# - OF we kleuren dan alle lessen die niet bij dit vak horen wit? Nee want ook tekst etc...
-
-# Buitenste assen omdraaien
-
 # alle code mooier schrijven + comments
-
-# Pushen
+# eval_schedule_objects() eruit?
