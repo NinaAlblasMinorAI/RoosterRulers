37cb3fb766fb34623cf261cf044a8846344e292e
Marc Jurriens <marc_uva@DESKTOP-6Q1LGFB.localdomain>
2022-06-28 11:39:53 +0200

    changes to main

--- main.py
+++ main.py
@@ -13,17 +13,38 @@ import sys
 import math
 
 
-def main(algorithm, nr_runs, nr_repeats, nr_outer_repeats, nr_inner_repeats, temperature, nr_courses, verbose):
+def main():
 
-    # set the time and date for output files
+    # set-up parsing command line arguments
+    parser = argparse.ArgumentParser(description='Create a university schedule')
+
+    # adding arguments
+    parser.add_argument("algorithm", help="algorithm to fill the schedule")
+    parser.add_argument("-n", type=int, default=1, dest="nr_runs", help="number of runs")
+    parser.add_argument("-r", type=int, default=10, dest="nr_repeats", help="number of repeats")
+    parser.add_argument("-o", type=int, default=10, dest="nr_outer_repeats", help="number of outer repeats for redistribute students")
+    parser.add_argument("-i", type=int, default=10, dest="nr_inner_repeats", help="number of inner repeats for redistribute students")
+    parser.add_argument("-c", type=int, default=10, dest="nr_courses", help="number of courses to further divide into lessons")
+    parser.add_argument("-t", type=float, default=1, dest="temperature", help="simulated annealing start temperature")
+    parser.add_argument("-v", default=False, dest="verbosity", help="increase output verbosity", action="store_true")
+
+    # read arguments from command line
+    args = parser.parse_args()
+
+    algorithm = args.algorithm
+    nr_runs = args.nr_runs
+    nr_repeats = args.nr_repeats
+    nr_outer_repeats = args.nr_outer_repeats
+    nr_inner_repeats = args.nr_inner_repeats
+    temperature = args.temperature
+    nr_courses = args.nr_courses
+    verbose = args.verbosity
+        
+    # create a log file with current date and time
     now = datetime.now()
     dt_string = now.strftime("%d_%m_%Y_%H_%M")
-
-    # create a log file with current date and time
     logfile = open(f"output_data/log_{algorithm}_{dt_string}.txt", "w")  
-    if algorithm == "hillclimber":
-        box_plot_points_file = open(f"output_data/box_plot_points_{algorithm}.txt", "w")      
-
+   
     # lists to store points for box plot and line graph
     boxplot_points = []
     linegraph_points = []
@@ -43,43 +64,38 @@ def main(algorithm, nr_runs, nr_repeats, nr_outer_repeats, nr_inner_repeats, tem
 
             # shuffle lessons based on specified algorithm and save points
             print(f"Starting lesson {algorithm} run {i + 1}.....")
-            lesson_swap = RedistributeLessons(algorithm, schedule, nr_repeats, temperature, verbose)
-            schedule, points = lesson_swap.get_schedule(), lesson_swap.get_points()
-            linegraph_points.extend(points)
-            malus_points = schedule.eval_schedule()
-            logfile.write(f"Intermediate result after shuffling lessons: {malus_points}\n")
+            lesson_swap = RedistributeLessons(algorithm, schedule, nr_repeats, temperature, verbose) 
+            linegraph_points.extend(lesson_swap.get_points())
+            schedule = lesson_swap.get_schedule()
+            logfile.write(f"Intermediate result after shuffling lessons: {schedule.eval_schedule()}\n")
 
             # shuffle students between lessons with hillclimber and save points
             print(f"Starting student hillclimber run {i + 1}.....")
             student_swap = RedistributeStudents("hillclimber", schedule, nr_outer_repeats, nr_inner_repeats, verbose)
-            schedule, points = student_swap.get_schedule(), student_swap.get_points()
-            linegraph_points.extend(points)
-            malus_points = schedule.eval_schedule()
-            logfile.write(f"Intermediate result after shuffling students: {malus_points}\n")
+            linegraph_points.extend(student_swap.get_points())
+            schedule = student_swap.get_schedule()
+            logfile.write(f"Intermediate result after shuffling students: {schedule.eval_schedule()}\n")
 
             # redistribute courses in lessons with greedy and save points
             print(f"Starting course greedy run {i + 1}.....")
             course_split = RedistributeCourses("greedy", schedule, nr_courses, verbose)
-            schedule, points = course_split.get_schedule(), course_split.get_points()
-            linegraph_points.extend(points)
-            malus_points = schedule.eval_schedule()
-            logfile.write(f"Intermediate result after redistributing courses: {malus_points}\n")
+            linegraph_points.extend(course_split.get_points())
+            schedule = course_split.get_schedule()
+            logfile.write(f"Intermediate result after redistributing courses: {schedule.eval_schedule()}\n")
 
             # shuffle lessons based on specified algorithm and save points
             print(f"Starting lesson {algorithm} run {i + 1}.....")
             lesson_swap = RedistributeLessons(algorithm, schedule, nr_repeats, temperature, verbose)
-            schedule, points = lesson_swap.get_schedule(), lesson_swap.get_points()
-            linegraph_points.extend(points)
-            malus_points = schedule.eval_schedule()
-            logfile.write(f"Intermediate result after shuffling lessons: {malus_points}\n")
+            linegraph_points.extend(lesson_swap.get_points())
+            schedule = lesson_swap.get_schedule()
+            logfile.write(f"Intermediate result after shuffling lessons: {schedule.eval_schedule()}\n")
 
             # shuffle students between lessons with hillclimber and save points
             print(f"Starting student hillclimber run {i + 1}.....")
             student_swap = RedistributeStudents("hillclimber", schedule, nr_outer_repeats, nr_inner_repeats, verbose)
-            schedule, points = student_swap.get_schedule(), student_swap.get_points()
-            linegraph_points.extend(points)
-            malus_points = schedule.eval_schedule()
-            logfile.write(f"Intermediate result after shuffling students: {malus_points}\n")
+            linegraph_points.extend(student_swap.get_points())
+            schedule = student_swap.get_schedule()
+            logfile.write(f"Intermediate result after shuffling students: {schedule.eval_schedule()}\n")
 
         # compute malus points of schedule
         malus_points = schedule.eval_schedule()
@@ -88,6 +104,7 @@ def main(algorithm, nr_runs, nr_repeats, nr_outer_repeats, nr_inner_repeats, tem
         # save the schedule if it is the best schedule so far
         if malus_points < best_result:
             best_result = malus_points
+            best_results = []
             best_results.append(schedule)
 
         # print the malus points to the log file
@@ -99,20 +116,13 @@ def main(algorithm, nr_runs, nr_repeats, nr_outer_repeats, nr_inner_repeats, tem
         # if the schedule is valid, add the malus points to a the list of results
         if malus_points:
             boxplot_points.append(malus_points)
+    
+    # close the log file
+    logfile.close()
 
-    # create a box plot of the results
-    if algorithm == "hillclimber":
-        for point in boxplot_points:
-            box_plot_points_file.write(f"{point}\n")
-
-    else:
-        hc_boxplot_points = []
-        with open("output_data/box_plot_points_hillclimber.txt", "r") as f:
-            for line in f:
-                hc_boxplot_points.append(int(line[:-1]))
-        
-        visualize_box_plot(hc_boxplot_points, boxplot_points, N=nr_runs, T=temperature, R1=nr_repeats, R2=nr_outer_repeats, R3=nr_inner_repeats)
-        print(f"{dt_string}_box_plot.png created in folder output_data")
+    # write the output files
+    now = datetime.now()
+    dt_string = now.strftime("%d_%m_%Y_%H_%M")
 
     # actions for the hillclimber and simulated annealing runs
     if algorithm == "hillclimber" or algorithm == "simulated_annealing":
@@ -121,47 +131,34 @@ def main(algorithm, nr_runs, nr_repeats, nr_outer_repeats, nr_inner_repeats, tem
         visualize_iterative(linegraph_points, algorithm)
         print(f"{algorithm}_{dt_string}_plot.png created in folder output_data")
         
-        # get the best schedule
-        schedule = best_results[0] 
-        
         # store the best schedule in a pickle file
         sys.setrecursionlimit(2000)
         pickle_output_file = open(f"output_data/pickled_schedule_{algorithm}_{dt_string}.pickle", "wb")
         pickle.dump(schedule, pickle_output_file)
+        pickle_output_file.close()
             
         # make a bokeh visualization of the best schedule
         visualize_schedule(schedule, f"output_data/{algorithm}_{dt_string}_schedule.html")
         print(f"{algorithm}_{dt_string}_schedule.html created in folder output_data")
 
-        # close the pickle file
-        pickle_output_file.close()
+        if algorithm == "hillclimber":
+            box_plot_points_file = open(f"output_data/box_plot_points_{algorithm}.txt", "w")  
+            box_plot_points_file.write(f"{point}\n")
+            box_plot_points_file.close()
 
-    # close the log file
-    logfile.close()
+        
+        # else:
+        #     hc_boxplot_points = []
+        #     with open("output_data/box_plot_points_hillclimber.txt", "r") as f:
+        #         for line in f:
+        #             hc_boxplot_points.append(int(line[:-1]))
+            
+        visualize_box_plot(hc_boxplot_points, boxplot_points, N=nr_runs, T=temperature, R1=nr_repeats, R2=nr_outer_repeats, R3=nr_inner_repeats)
+        print(f"{dt_string}_box_plot.png created in folder output_data")
 
-    if algorithm == "hillclimber":
-        box_plot_points_file.close()
+                    
 
 
 if __name__ == "__main__":
 
-    # set-up parsing command line arguments
-    parser = argparse.ArgumentParser(description='Create a university schedule')
-
-    # adding arguments
-    parser.add_argument("algorithm", help="algorithm to fill the schedule")
-    parser.add_argument("-n", type=int, default=1, dest="nr_runs", help="number of runs")
-    parser.add_argument("-r", type=int, default=10, dest="nr_repeats", help="number of repeats")
-    parser.add_argument("-o", type=int, default=10, dest="nr_outer_repeats", help="number of outer repeats for redistribute students")
-    parser.add_argument("-i", type=int, default=10, dest="nr_inner_repeats", help="number of inner repeats for redistribute students")
-    parser.add_argument("-c", type=int, default=10, dest="nr_courses", help="number of courses to further divide into lessons")
-    parser.add_argument("-t", type=int, default=1, dest="temperature", help="simulated annealing start temperature")
-    parser.add_argument("-v", default=False, dest="verbosity", help="increase output verbosity", action="store_true")
-
-    # read arguments from command line
-    args = parser.parse_args()
-
-    # run main with provided arguments
-    main(args.algorithm, args.nr_runs, args.nr_repeats, 
-        args.nr_outer_repeats, args.nr_inner_repeats, 
-        args.temperature, args.nr_courses, args.verbosity)
+    main()
